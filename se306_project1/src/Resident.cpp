60cc3b877307289bd2bf5f8ef7451f55af3bc660
Harriet <harrietrc@gmail.com>
2014-08-13 17:42:19 +1200

    Moved periodic callback and navigation to resident

--- se306_project1/src/Resident.cpp
+++ se306_project1/src/Resident.cpp
@@ -6,126 +6,300 @@
 
 #include <sstream>
 #include "math.h"
+#include <cmath>
+#include <stdlib.h>
+#include "time_conversion.hpp"
 
-Class Resident : public Agent {
-	//velocity of the robot
-	double linear_x;
-	double angular_z;
+//velocity of the robot
+double linear_x;
+double angular_z;
+
+//goal pose and orientation
+double goal_x;
+double goal_y;
+double goal_angle;
+
+bool running = false;
+bool isSet = false;
+
+//current pose and orientation of the robot
+double px;
+double py;
+double cur_angle;
+
+int cc = 1; //current_checkpoint = 0;
+
+bool is_called = true; 
+
+std::pair<double,bool> goal_pair;
+std::pair<double, double> ret;	
+
+int checkpoints[5][2] = {  
+{30, 25}, 
+{30, 7}, 
+{43, 7},
+{30, 7},
+{30, 25}  
+};
+
+double kitchen[2] = {43,7};
+double kitchen_corner[2] = {30, 7};
+double home_centre[2] = {30, 25};
+double bed_corner[2] = {32, 40};
+double toilet[2] = {10,40};
+
+std::pair<double, double> move(double goal_x, double goal_y, double cur_angle, double goal_angle, double px, double py);
+std::pair<double,bool> calc_goal(double goal_x, double goal_y, double cur_angle, double px, double py); 
+void StageOdom_callback(nav_msgs::Odometry msg); 
+
+
+void StageOdom_callback(nav_msgs::Odometry msg)
+{
+	if (running ) {
+		return;
+	}
+	running = true;
+	ret = std::make_pair(0, 0); //initialize pair. Used to get return.
+	//double val = 0;
+
+	//Converting from quaternion to radians
+	cur_angle = acos(msg.pose.pose.orientation.w) * 2;
+	if (msg.pose.pose.orientation.z > 0) {
+		cur_angle = (2*M_PI)-acos(msg.pose.pose.orientation.w) * 2;
+	}
+
+	//Rounding to 3 decimal places
+	cur_angle = ((int)(cur_angle * 1000 + .5) / 1000.0);
 	
-	//pose of the robot
-	double px;
-	double py;
-	double theta;
+	//Update the current position
+	px = msg.pose.pose.position.x + checkpoints[0][0];
+	py = msg.pose.pose.position.y + checkpoints[0][1];
 	
-	// Enumeration of type of robot
-	enum Type{FRIEND, RELATIVE, DOCTOR, NURSE, CAREGIVER, ASSISTANT, RESIDENT};
+	//ROS_INFO("angular z: %f", angular_z);
+ROS_INFO("---------------------------------------------");
+	ROS_INFO("px: %f", px);
+	ROS_INFO("py: %f", py);
+	ROS_INFO("goal x: %f", goal_x);
+	ROS_INFO("goal y: %f", goal_y);
+	ROS_INFO("Current angle: %f", cur_angle);
+	ROS_INFO("Goal theta:   %f", goal_angle);
+ROS_INFO("---------------------------------------------");
+
+
+	//When goal reached
+	if ((px <= goal_x + 0.5) && (px >= goal_x - 0.5) && (py <= goal_y + 0.5) && (py >= goal_y - 0.5)) {
 	
-	int robot_id;
+		if (cc == 4) { //If at last checkpoint
+			linear_x = 0;
+			
+		} else {
+			cc++; //Increment checkpoint index
+		}
+		goal_x = checkpoints[cc][0];
+		goal_y = checkpoints[cc][1];
+
+		if (is_called) {
+			//Adjusts for delay between publish and subscribe	
+			//val = goal_angle;
+			goal_pair = calc_goal(goal_x, goal_y, cur_angle, px, py);
+			goal_angle = goal_pair.first;
+			//if (goal_pair.second == false) {
+			//	goal_angle = val + goal_pair.first;
+			//} else {
+			//	if(goal_pair.first >= val){
+			//		goal_angle = 6.283 + val - goal_pair.first;
+			//	} else {			
+			//		goal_angle = val - goal_pair.first;
+			//	}
+			//}
+			is_called = false;
+		}
+
+		if (goal_angle >= 6.283) {
+			goal_angle = goal_angle - 6.283;
+		}
+		ret = move(goal_x, goal_y, cur_angle, goal_angle, px, py);
+				linear_x = ret.first;
+		angular_z = ret.second;	
+	} else { //Do this until goal is reached
+		
+		is_called = true;
+		//val = 0;
+		ret = move(goal_x, goal_y, cur_angle, goal_angle, px, py);	
+		linear_x = ret.first;
+		angular_z = ret.second;
+	}
+
+	running = false;
+}
+
+
+//Keeps robot moving by changing linear_x and angular_z
+std::pair<double, double> move(double goal_x, double goal_y, double cur_angle, double goal_angle, double px, double py) 
+{	
+ //contains linear_x and angular_x. Defines how robot movees between goals.	
 	
-	// Enum or string? to be specified
-	String status;
-	
-	int health = 100;
-	int boredom = 100;
-	void StageOdom_callback(nav_msgs::Odometry msg)
-	{
-		//This is the call back function to process odometry messages coming from Stage. 	
-		px = 5 + msg.pose.pose.position.x;
-		py =10 + msg.pose.pose.position.y;
-		ROS_INFO("Current x position is: %f", px);
-		ROS_INFO("Current y position is: %f", py);
+	std::pair<double,double>_ret = std::make_pair(0, 0); //initialize pair. Used to get return.
+	double moveSpeed = M_PI/2;
+	moveSpeed = ((int)(moveSpeed * 1000 + .5) / 1000.0);
+	//When the robot is facing the correct direction, start moving
+
+	double threshold = cur_angle-moveSpeed/10;
+	threshold = ((int)(threshold * 1000 + .5) / 1000.0);
+
+	if ((goal_angle  == threshold) || isSet) {
+		_ret.first = 3; //linear_x
+		_ret.second = 0; //angular_z
+		isSet = true;
+	} else {
+		_ret.first = 0; //linear_x
+		_ret.second = moveSpeed; //angular_z
+		isSet = false;
 	}
 	
-	void StageLaser_callback(sensor_msgs::LaserScan msg)
-	{
-		//This is the callback function to process laser scan messages
-		//you can access the range data from msg.ranges[i]. i = sample number
+	//if (goal_pair.second) {
+	//	_ret.second = _ret.second * -1;
+	//}
+
+	//When robot reaches the goal, stop moving (with a leeway of 0.4)
+	
+	ROS_INFO("##################");
+	ROS_INFO("goal_y: %f",goal_y);
+	ROS_INFO("py: %f",py);
+	ROS_INFO("goal_x: %f",goal_x);
+	ROS_INFO("px: %f",px);
+	ROS_INFO("##################");
+	if ((px <= goal_x + 0.5) && (px >= goal_x - 0.5) && (py <= goal_y + 0.5) && (py >= goal_y - 0.5)) {	
+			_ret.first = 0; //linear_x
+			isSet = false;
+			ROS_INFO("are you in yet? the sequel");
 		
 	}
+
+	return _ret; 
+}
+
+std::pair <double,bool> calc_goal(double goal_x, double goal_y, double cur_angle, double px, double py) 
+{
+
+	std::pair <double,bool> _ret (0, false);
+
+	//Initial and goal vectors used to calculate goal theta
+	double init_vector_x;
+	double init_vector_y;
+	double goal_vector_x;
+	double goal_vector_y;
+	//double dot;
+	double goal_angle;
+	double cross = 0;
+	bool is_clockwise = false;
 	
-	int main(int argc, char **argv)
-	{
+
+	//Finding the vector that the robot is facing and the goal vector
+	init_vector_x = cos(cur_angle);
+	init_vector_y = sin(cur_angle);
+	goal_vector_x = goal_x - px;
+	goal_vector_y = goal_y - py;
+
+	cross = (init_vector_x * goal_vector_y) - (goal_vector_x * init_vector_y);
 	
-	 //initialize robot parameters
-		//Initial pose. This is same as the pose that you used in the world file to set	the robot pose.
-		theta = M_PI/2.0;
-		px = 10;
-		py = 20;
-		
-		//Initial velocity
-		linear_x = 0.2;
-		angular_z = 0.2;
-		
+	if (cross < 0) {
+		is_clockwise = true;
+	}
+	else{
+		is_clockwise = false;
+	}
+	
+	goal_angle = atan2(goal_vector_y, goal_vector_x); //pi <= goal_angle < -pi
+	if (goal_angle < 0) {
+		goal_angle = 2 * M_PI + goal_angle; //Remove sign, then add to pi
+	} else if (goal_angle == 2 * M_PI) { //New goal angle =>   >0 to 6.283
+		goal_angle = 0;
+	}
+	goal_angle = (2* M_PI) - goal_angle;
+
+	//rounding goal_angle to three decimal places
+	goal_angle = ((int)(goal_angle * 1000 + .5) / 1000.0);
+
+	_ret.first = goal_angle;
+	_ret.second = is_clockwise;
+
+	return _ret;
+}
+
+/*
+	Simple model of resident - resident should move to a random checkpoint every once in a while. Will be replaced with a
+	more complex and realistic model later.
+*/
+void randomCheckpointCallback(const ros::TimerEvent&) {
+	
+}
+
+int main(int argc, char **argv)
+{
+
+ //initialize robot parameters
+
+	goal_pair = std::make_pair(0, false);
+
+	//Initial pose. This is same as the pose that you used in the world file to set	the robot pose.
+	px = checkpoints[cc-1][0];
+	py = checkpoints[cc-1][1];
+	cur_angle = 0;
+
+	//Goal 
+	goal_x = checkpoints[cc][0];
+	goal_y = checkpoints[cc][1];
+
+	goal_pair = calc_goal(goal_x, goal_y, cur_angle, px, py);
+	goal_angle = goal_pair.first;	
+
+	//Initial velocity
+	linear_x = 0;
+	angular_z = 0.001;
+	
+	//Align local system to global coordinates
+	
 	//You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
 	ros::init(argc, argv, "RobotNode0");
-	
+
 	//NodeHandle is the main access point to communicate with ros.
 	ros::NodeHandle n;
-	
+
 	//advertise() function will tell ROS that you want to publish on a given topic_
 	//to stage
 	ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000); 
-	
+
 	//subscribe to listen to messages coming from stage
 	ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_0/odom",1000, StageOdom_callback);
-	ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_0/base_scan",1000,StageLaser_callback);
-	
-	ros::Rate loop_rate(10);
-	
+
+	ros::Rate loop_rate(1000);
+
 	//a count of howmany messages we have sent
 	int count = 0;
-	
+
 	////messages
 	//velocity of this RobotNode
 	geometry_msgs::Twist RobotNode_cmdvel;
-	
+
+	// Periodic callback
+	int dur2 = time_conversion::simHoursToRealSecs(2); // Perform callback every 2 simulation hours
+	ros::Timer medicationTimer = n.createTimer(ros::Duration(dur2), randomCheckpointCallback); 
+
 	while (ros::ok())
 	{
 		//messages to stage
 		RobotNode_cmdvel.linear.x = linear_x;
 		RobotNode_cmdvel.angular.z = angular_z;
-	        
 		//publish the message
 		RobotNode_stage_pub.publish(RobotNode_cmdvel);
 		
 		ros::spinOnce();
-	
+
 		loop_rate.sleep();
 		++count;
 	}
-	
-	return 0;
-	
-	}
-	
-	// Return type of robot
-	Type get_Type(){
-	
-	}
-	
-	// Get id of robot
-	int get_id(){
-	
-	}
-	
-	// Wakes up
-	void wake_up(){
-		
-	}
-	
-	// Eat
-	void eat(){
-	
-	}
-	
-	// Takes medicine
-	void take_medicine(){
-	
-	}
-	
-	// Accepts entertainment
-	void accept_entertainment(){
-	
-	}
 
-};
+	return 0;
+
+}
