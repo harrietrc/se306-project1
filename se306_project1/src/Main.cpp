6a4c10fdeb4b41c018f385dd9ec13b4dd97935f0
John <clee842@aucklanduni.ac.nz>
2014-08-14 16:35:59 +1200

    merge

--- se306_project1/src/Main.cpp
+++ se306_project1/src/Main.cpp
@@ -13,9 +13,6 @@ int main(int argc, char** argv) {
 	AgentFactory agentFactory;
 	nodeList[agentType] = nodeList[agentType] + agentFactory.createAgent(agentType);
 
-	agentType = AgentConst::ASSISTANT;
-	nodeList[agentType] = nodeList[agentType] + agentFactory.createAgent(agentType);
-
 	std::cout << "now we have " << nodeList[agentType] << "resident(s)\n";
 
 	agentType = AgentConst::ASSISTANT;
