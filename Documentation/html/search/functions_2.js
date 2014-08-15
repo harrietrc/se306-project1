d3f60c47454988fd3fcdbcdb68c93b63b2cf9b7c
Harriet <harrietrc@gmail.com>
2014-08-15 12:54:08 +1200

    Added file for customised index.html (doxygen)

--- Documentation/html/search/functions_2.js
+++ Documentation/html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['calc_5fgoal_5fangle',['calc_goal_angle',['../classAssistant.html#a8fce345025f32bcd0e45d83960d4b3f7',1,'Assistant::calc_goal_angle()'],['../classDoctor.html#a55fe6c06a3ce07fd9014a03000df43db',1,'Doctor::calc_goal_angle()'],['../classResident.html#ae8e00862499c8a502b5c99f8267d3345',1,'Resident::calc_goal_angle()']]]
+  ['calc_5fgoal_5fangle',['calc_goal_angle',['../classAssistant.html#a8fce345025f32bcd0e45d83960d4b3f7',1,'Assistant::calc_goal_angle()'],['../classDoctor.html#a55fe6c06a3ce07fd9014a03000df43db',1,'Doctor::calc_goal_angle()'],['../classResident.html#ae8e00862499c8a502b5c99f8267d3345',1,'Resident::calc_goal_angle()']]],
+  ['createmockagent',['createMockAgent',['../classAgentFactory.html#a8785d71f067068f09326b4c5af5007b4',1,'AgentFactory']]]
 ];
