68b8418723d1e156b9309c4719abc4828bd238c2
eatyourhummus247 <mshe246@gmail.com>
2014-08-20 10:29:03 +1200

    Made feature/pq branch for priority Queue and added initial files

---
+++ se306_project1/src/PriorityQueue.cpp
@@ -0,0 +1,34 @@
+/*#include <string>
+#include <queue>
+#include <iostream>
+#include <stddef.h>
+#include "PriorityQueue.h"
+
+
+// Global static pointer to ensure that there is only ever one
+// instance of this Queue
+PriorityQueue* PriorityQueue::myQueue = NULL; 
+
+
+PriorityQueue* PriorityQueue::Instace()
+{
+	if(!myQueue) {
+		myQueue = new PriorityQueue;
+	}
+
+	return myQueue;
+}
+
+
+string getValue() {
+	if (!value) {
+		value = "Value is no longer empty";
+	}
+	return value;
+}
+
+
+void changeValue(string newVal) {
+	value = newVal;
+}
+*/
---
+++ se306_project1/src/PriorityQueue.hpp
@@ -0,0 +1,13 @@
+/*#include <string>
+
+class PriorityQueue{
+public:
+	static PriorityQueue* Instance();
+	string getValue();
+	void changeValue(string newVal);
+private:
+	PriorityQueue(){}; // Constructor private so it can't be called
+	PriorityQueue(PriorityQueue const&){}; 
+	static PriorityQueue* myQueue; // operater
+	string value;
+}*/
