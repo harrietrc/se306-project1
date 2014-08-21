269b5b27d213fde4a2570b18845a586cca4b4ebf
Govindu <govindu7@hotmail.com>
2014-08-21 18:08:25 +1200

    Partially completed do and delegate methods for caregiver.

--- se306_project1/src/Caregiver.cpp
+++ se306_project1/src/Caregiver.cpp
@@ -3,36 +3,94 @@
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/LaserScan.h>
-
 #include <sstream>
 #include "math.h"
 #include "Caregiver.h"
 
+
 /**
-*	@brief Updates the Caregiver's x position, y position, and angle to reflect its current pose.
-*	@note Rounding is used to calculate the current angle. This approximation is accounted for by using threshholds when processing angles.
-*	@param msg Odometry message from odom topic
+*	@brief Callback function that unpacks and processes resident status messages.
+*	Assistant should subscribe to the ResidentMsg topic in order for this callback to be called. ResidentMsg is published by the Resident.
+*	@note Currently this callback processes only resident hunger, controlling the cooking behaviour. More behaviours 
+*	can be implemented later.
+*	@param msg A custom ResidentMsg message that contains information about the resident's current status.
 */
-void Caregiver::StageOdom_callback(nav_msgs::Odometry msg)
+void Caregiver::delegate(se306_project1::ResidentMsg r_msg, se306_project1::AssistantMsg a_msg)
 {
-	//This is the call back function to process odometry messages coming from Stage. 	
-	px = 5 + msg.pose.pose.position.x;
-	py =10 + msg.pose.pose.position.y;
-	ROS_INFO("Current x position is: %f", px);
-	ROS_INFO("Current y position is: %f", py);
+	if (r_msg.hygiene < 50) { // Resident is in dire need of a shower
+		doShowerSupport(r_msg);
+	}
+	else if (r_msg.mood <50} { // Resident is demoralized, give him his depression medication
+		doMoralSupport(r_msg);
+	}
+	else if (r_msg.fitness) { // Resident needs excercise
+		doExerciseSupport(r_msg);
+	}
+	else if (a_msg.FoodDelivered) {
+		doEatSupport(r_msg)
+	}
+}
+
+
+/**
+*	@brief Caregiver helps the resident to eat. Will feed him after his meal has been prepared by an assistant.
+*	@return Returns true if behaviour was successful, and false otherwise.
+*/
+bool Caregiver::doEatSupport(se306_project1::ResidentMsg msg) {
+
+	if(needs_food){
+		needs_food = false;
+		//Caregiver should move to residents position in order to "feed" him
+	}
+
+	if ((sqrt(pow((msg.x - px),2) + pow((msg.y - py),2)) < 2.5) && msg.hunger == 100){
+		//feed complete, now b (leave the house)
+		return true;
+	}
+	return false;
 }
 
 /**
-*	@brief Callback function to process laser scan messsages.
-*	You can access the range data from msg.ranges[i]. i = sample number
-*	@note Currently blank as it is not in use. Navigation operates through a checkpoint system.
-*	@param msg Single scan from a planar laser range finder
+*	@brief Caregiver helps the resident to do exercise. Will arrive when excercise scheduled.
+*	@return Returns true if behaviour was successful, and false otherwise.
 */
-void Caregiver::StageLaser_callback(sensor_msgs::LaserScan msg)
-{
-	//This is the callback function to process laser scan messages
-	//you can access the range data from msg.ranges[i]. i = sample number
-	
+bool Caregiver::doExerciseSupport(se306_project1::ResidentMsg msg) {
+		if(needs_excercise){
+		needs_excercise = false;
+		//Caregiver should move to residents position, then help him excercise. Go to gym area etc
+	}
+
+	if ((sqrt(pow((msg.x - px),2) + pow((msg.y - py),2)) < 2.5) && msg.excecise == true){
+		//excercise complete, leave house
+		return true;
+	}
+	return false;
+}
+
+/**
+*	@brief Caregiver helps the resident to shower.
+*	@return Returns true if behaviour was successful, and false otherwise.
+*/
+bool Caregiver::doShowerSupport(se306_project1::ResidentMsg msg) {
+	if(needs_shower){
+		needs_shower = false;
+		//Caregiver should move to residents position, take him to the shower(spin around) and clean him
+	}
+
+	if ((sqrt(pow((msg.x - px),2) + pow((msg.y - py),2)) < 2.5) && msg.hygiene == 100){
+		//shower complete, take resident to previous position and leave house
+		return true;
+	}
+	return false;
+}
+
+/**
+*	@brief Caregiver gives the resident moral support.
+*	@return Returns true if behaviour was successful, and false otherwise.
+*/
+bool Caregiver::doMoralSupport(se306_project1::ResidentMsg msg) {
+	//No idea what this is. Go hug him or something
+	return true;
 }
 
 /**
@@ -41,15 +99,7 @@ void Caregiver::StageLaser_callback(sensor_msgs::LaserScan msg)
 */
 int Caregiver::run(int argc, char *argv[])
 {
-	//initialize robot parameters
-	//Initial pose. This is same as the pose that you used in the world file to set	the robot pose.
-	theta = M_PI/2.0;
-	px = 10;
-	py = 20;
-	
-	//Initial velocity
-	linear_x = 0.2;
-	angular_z = 0.2;
+	/* -- Initialisation -- */
 	
 	//You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
 	ros::init(argc, argv, "Caregiver");
@@ -57,22 +107,34 @@ int Caregiver::run(int argc, char *argv[])
 	//NodeHandle is the main access point to communicate with ros.
 	ros::NodeHandle n;
 
+	ros::Rate loop_rate(10);
+
+	//Booleans which determine if the caregiver should care for the resident
+	needs_food = false;
+	needs_excercise = false;
+	needs_shower = false;
+	needs_moral_support = false;
+
+
+	/* -- Publish / Subscribe -- */
+
 	//advertise() function will tell ROS that you want to publish on a given topic_
 	//to stage
 	ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000); 
 
 	//subscribe to listen to messages coming from stage
 	ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_0/odom",1000, &Caregiver::StageOdom_callback,this);
-	ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_0/base_scan",1000,&Caregiver::StageLaser_callback,this);
 
-	ros::Rate loop_rate(10);
+	//custom Resident subscriber to "resident/state"
+	ros::Subscriber resident_sub = n.subscribe<se306_project1::ResidentMsg>("residentStatus",1000,&Caregiver::delegate, this);
 
-	//a count of howmany messages we have sent
-	int count = 0;
+
+	ros::Subscriber assitant_sub = n.subscribe<se306_project1::AssistantMsg>("assistantStatus",1000,&Caregiver::delegate, this);
 
 	////messages
 	//velocity of this RobotNode
 	geometry_msgs::Twist RobotNode_cmdvel;
+	
 
 	while (ros::ok())
 	{
@@ -84,13 +146,10 @@ int Caregiver::run(int argc, char *argv[])
 		RobotNode_stage_pub.publish(RobotNode_cmdvel);
 		
 		ros::spinOnce();
-
 		loop_rate.sleep();
-		++count;
 	}
 
 	return 0;
-
 }
 
 /**
@@ -100,3 +159,4 @@ int main(int argc, char *argv[]) {
 	Caregiver *a = new Caregiver();
 	a->Caregiver::run(argc, argv);
 }
+
--- se306_project1/src/Caregiver.h
+++ se306_project1/src/Caregiver.h
@@ -5,10 +5,23 @@
 */
 class Caregiver : public Visitor
 {
+	protected:
+		bool needs_food;
+		bool needs_excercise;
+		bool needs_shower;
+		bool needs_moral_support;
+		
 	public:
 		void StageOdom_callback(nav_msgs::Odometry msg);
 		void StageLaser_callback(sensor_msgs::LaserScan msg);
 		int run(int argc, char *argv[]);
+
+		void delegate(se306_project1::ResidentMsg msg);
+
+		bool doEatSupport(se306_project1::ResidentMsg msg);
+		bool doMoralSupport(se306_project1::ResidentMsg msg);
+		bool doShowerSupport(se306_project1::ResidentMsg msg);
+		bool doExcerciseSupport(se306_project1::ResidentMsg msg);
 		
 		// Get id of robot
 		//int get_id()
