45e2da749a6442ee977719f9a86f5aad7b6ecbdb
Harriet Robinson-Chen <harrietrc@gmail.com>
2014-08-27 09:25:07 +1200

    Committing to pull - created files for graph class

---
+++ se306_project1/src/CheckPointGraph.hpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "boost/graph/adjacency_list.hpp"
+#include <boost/graph/graphviz.hpp> // Good for debugging, but take out for final build.
+#include "boost/graph/breadth_first_search.hpp"
+
+class CheckPointGraph {
+	
+};
