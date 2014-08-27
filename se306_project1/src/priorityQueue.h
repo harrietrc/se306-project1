b77b9687a91fbe00b7c74b184a4024f7f47b63fe
Mshehadeh <mshe246@gmail.com>
2014-08-27 14:48:16 +1200

    Reimplemented PQ

---
+++ se306_project1/src/priorityQueue.h
@@ -0,0 +1,26 @@
+/*
+ * priorityQueue.h
+ *
+ *  Created on: Aug 27, 2014
+ *      Author: mustafa
+ */
+
+#ifndef PRIORITYQUEUE_H_
+#define PRIORITYQUEUE_H_
+
+
+enum residentStates {hunger,healthLow,bored,emergency,tired,caregiver,friends,idle};
+
+
+class priorityQueue {
+public:
+	priorityQueue();
+	virtual ~priorityQueue();
+	void addToPQ(residentStates currentState);
+	residentStates popFromPQ();
+private:
+	bool isStateInPQ(residentStates currentState);
+
+};
+
+#endif /* PRIORITYQUEUE_H_ */
