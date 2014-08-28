26a563f32607410797872bdef87e66c1da40e30c
John <clee842@aucklanduni.ac.nz>
2014-08-29 03:39:38 +1200

    testcase for global dummy method in pq.cpp

--- se306_project1/test/utest.cpp
+++ se306_project1/test/utest.cpp
@@ -3,6 +3,7 @@
 // Bring in gtest
 //#include "../src/"
 #include <gtest/gtest.h>
+#include "../src/priorityQueue.cpp"
 
 int addVars(int a, int b) {
 	return a + b;
@@ -20,6 +21,10 @@ TEST(TestSuite, testCase2)
 	EXPECT_EQ(4, addVars(2,3));
 }
 
+TEST(TestSuite, testPQDummyMethod)
+{
+	EXPECT_EQ(1, somePQDummyTest(1,2));
+}
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
